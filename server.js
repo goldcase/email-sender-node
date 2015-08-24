dddd5c8097565eee34867f935a2f3a474e6f3612
Johnny Chang <johnny@johnnychang.com>
2015-08-24 07:11:33 -0500

    initial commit; basic express app; install dependencies

---
+++ server.js
@@ -0,0 +1,7 @@
+var express    = require("express");
+var nodemailer = require("nodemailer");
+var app        = express();
+
+app.listen(3000, function() {
+    console.log("Listening on port 3000");
+});
